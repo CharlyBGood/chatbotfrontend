1d5737f466b6e307d95fdfe4c250ec922895c641
charlybgood__x <bonavita.carlos@gmail.com>
2025-06-24 18:11:05 -0300

    fix logo width and height

--- src/components/faviconcomponent/ChatFavicon.jsx
+++ src/components/faviconcomponent/ChatFavicon.jsx
@@ -4,7 +4,7 @@ export function ChatFavicon({ className = '', alt = 'Favicon' }) {
       src="https://res.cloudinary.com/dr8pwzxzn/image/upload/c_thumb,w_200,g_face/v1741819729/logoSolo_qvxfcr.png"
       alt={alt}
       loading="lazy"
-      className={`w-6 h-5 ${className}`}
+      className={`w-6 h-4 ${className}`}
     />
   );
 }
